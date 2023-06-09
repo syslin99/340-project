0270af6003b5fd67adaa8225d752cdc04907793c
Joel Villamor <villamoj@flip2.engr.oregonstate.edu>
2023-06-08 22:26:36 -0700

    Update Sale Auto-Generates Products According to Sale

--- public/js/update_sale.js
+++ public/js/update_sale.js
@@ -126,3 +126,114 @@ function updateRow(data, saleID) {
     }
 
 }
+
+
+function populateSalesForm(products) {
+    var inputID = document.getElementById('input-id-update')
+    var idValue = inputID.value
+    var inputDate = document.getElementById('input-date-update')
+    var inputCust = document.getElementById('input-customer-update')
+    var inputEmp = document.getElementById('input-employee-update')
+
+    var salesTable = document.getElementById('sales-table')
+    var prodSalesTable = document.getElementById('product-sales-table')
+    var specDate = ''
+    var specCust = ''
+    var specEmp = ''
+
+    // Get the relevant data
+    for (let i = 0, row; row = salesTable.rows[i]; i++) {
+        if (salesTable.rows[i].getAttribute('data-value') == idValue) {
+            let specRow = salesTable.getElementsByTagName('tr')[i]
+            specDate = specRow.getElementsByTagName('td')[1].textContent
+            specCust = specRow.getElementsByTagName('td')[3].textContent
+            specEmp = specRow.getElementsByTagName('td')[4].textContent
+        }
+    }
+
+    // Clear table entries in preparation for new entries
+    let tableBody = document.getElementById('update-product-table-body')
+    let children = tableBody.children
+    for (var i = children.length - 1; i >= 0; i--) {
+        children[i].remove()
+    }
+    
+
+    var numProducts = 0
+    for (let i = 0, row; row = prodSalesTable.rows[i]; i++) {
+        specRow = prodSalesTable.getElementsByTagName('tr')[i]
+        rowEntries = specRow.children
+        
+        console.log(rowEntries.length)
+
+        // Continue search for sale id...
+        if (rowEntries[1].textContent == idValue) {
+            var prodName = rowEntries[2].textContent
+            var prodQuant = rowEntries[3].textContent
+            
+            // Add a table entry to the products table
+            addProductButton('update-product-table-body')
+            
+            // Populate the fields of the table
+            populateFields(numProducts, prodName, prodQuant, products)
+            numProducts += 1
+        }
+
+    }
+
+    // Populate the customer dropdown
+    let count = 0
+    for (var option of inputCust.options) {
+        if (specCust == option.innerHTML) {
+            inputCust.options[count].selected = true
+            break
+        } else {
+            inputCust.options[0].selected = true
+        }
+        count += 1
+    }
+
+    // Populate the employee dropdown
+    count = 0
+    for (var option of inputEmp.options) {
+        if (specEmp == option.innerHTML) {
+            inputEmp.options[count].selected = true
+            break
+        } else {
+            inputEmp.options[0].selected = true
+        }
+        count += 1
+    }
+
+    // Populate the inputs
+    inputDate.value = specDate
+}
+
+
+// Fills out the fields within each row
+function populateFields(rowIndex, name, quant, products) {
+
+    let tableBody = document.getElementById('update-product-table-body')
+    let tableRows = tableBody.children
+    let tableTds = tableRows[rowIndex].children
+    let prodSelect = tableTds[0].children[0]
+    let prodQuant = tableTds[2].children[0]
+
+    let count = 0
+    for (var option of prodSelect.options) {
+        if (name == option.innerHTML) {
+            prodSelect.options[count].selected = true
+            break
+        } else {
+            prodSelect.options[0].selected = true
+        }
+        count += 1
+    }
+
+    prodQuant.value = quant
+
+    // Use existing function to update the price field
+    updateUpProductCard(prodSelect, products)
+
+
+}
